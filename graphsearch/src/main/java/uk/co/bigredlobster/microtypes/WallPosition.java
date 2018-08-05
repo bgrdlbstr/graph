e7acbfdd1a79252f5b6741c58ff09d532082720a
Dave <lobster.spam.acct@gmail.com>
2018-08-05 21:51:13 +0100

    fix WallPosition dupe code

--- graphsearch/src/main/java/uk/co/bigredlobster/microtypes/WallPosition.java
+++ graphsearch/src/main/java/uk/co/bigredlobster/microtypes/WallPosition.java
@@ -1,38 +1,9 @@
 package uk.co.bigredlobster.microtypes;
 
-import java.util.Objects;
+import uk.co.bigredlobster.graph.shared.node.GridPosition;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-public class WallPosition {
-    private final PositionX x;
-    private final PositionY y;
-
+public class WallPosition extends GridPosition {
     public WallPosition(final PositionX x, final PositionY y) {
-        this.x = checkNotNull(x);
-        this.y = checkNotNull(y);
-    }
-
-    @Override
-    public String toString() {
-        return "WallPosition{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        WallPosition that = (WallPosition) o;
-        return Objects.equals(x, that.x) &&
-                Objects.equals(y, that.y);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(x, y);
+        super(x, y);
     }
 }
